db089f2ddf8bf53b700c6667246e85a9f0ab8a0a
Sarah Jacob <smjacob91@gmail.com>
2018-07-07 12:58:04 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,7 +7,17 @@ end
 
 
 
-
+ def key_for_min_value(name_hash)
+-	+ low = nil
++ low_name = nil
++ name_hash.each do |key, value|
++    if low == nil || low > value
++      low = value
++      low_name = key
++    end
++  end
++  low_name
+ end 	 end 
 
 
 
