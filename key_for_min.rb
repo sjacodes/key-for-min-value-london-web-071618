8156c50ea72232bdadc6af2dce06863c0af6e1af
Sarah Jacob <smjacob91@gmail.com>
2018-07-07 13:02:18 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,35 +2,14 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  
+  min_value = nil
+  min_key = nil
+  name_hash.each do |key, value|
+    if min_value == nil || min_value > value
+      min_value = value
+      min_key = key
+    end
+  end
+  return min_key
 end
 
-
-
- def key_for_min_value(name_hash)
--	+ low = nil
-+ low_name = nil
-+ name_hash.each do |key, value|
-+    if low == nil || low > value
-+      low = value
-+      low_name = key
-+    end
-+  end
-+  low_name
- end 	 end 
-
-
-
-  it "returns the key of the smallest hash value" do
-    expect(key_for_min_value({:blake => 500, :ashley => 2, :adam => 1})).to eq(:adam)
-  end
-
-  it "returns the key of the smallest hash value example 2" do
-    expect(key_for_min_value({:blake => 10, :ashley => 50, :adam => 17})).to eq(:blake)
-  end
-
-  it "returns nil for an empty hash" do
-    expect(key_for_min_value({})).to eq(nil)
-  end
-
-end
